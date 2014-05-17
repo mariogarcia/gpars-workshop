13b61ba5714ce2beeb1d59816125ec7f36aa8571
Mario Garcia <mario.ggar@gmail.com>
2014-05-17 21:30:48 +0200

    Added image processing example

--- src/main/groovy/exercises/forwarding/images/ImageProcessorActor.groovy
+++ src/main/groovy/exercises/forwarding/images/ImageProcessorActor.groovy
@@ -5,21 +5,25 @@ import static javax.imageio.ImageIO.write
 import static org.imgscalr.Scalr.resize
 import static java.io.File.createTempFile
 
+import exercises.test.NotificationActor
 import groovyx.gpars.actor.DefaultActor
 
-class ImageProcessorActor extends DefaultActor {
+class ImageProcessorActor extends NotificationActor {
+
+    ImageProcessorActor(DefaultActor monitor) {
+        super(monitor)
+    }
 
     void act() {
         loop {
             react { File image ->
-                println "Start processing $image"
                 def src = read(image)
                 def img = resize(src, 300, 200)
                 def tmp = new File(System.getProperty('java.io.tmpdir'))
 
                 write(img, 'jpg', new File(tmp, "processed_${image.name}"))
 
-                println "Image ${image.name} processed"
+                notifyIfMonitor(image.name)
             }
         }
     }
--- src/test/groovy/exercises/forwarding/images/ImagesProcessingSpecification.groovy
+++ src/test/groovy/exercises/forwarding/images/ImagesProcessingSpecification.groovy
@@ -1,31 +1,34 @@
 package exercises.forwarding.images
 
+import static groovyx.gpars.actor.Actors.actor
+
+import exercises.test.NotifiedActor
 import spock.lang.Specification
 
 class ImagesProcessingSpecification extends Specification {
 
+    final File RESOURCES_DIR = 'src/test/resources/exercises/forwarding/images/' as File
+    final File TEMPORARY_DIR = new File(System.getProperty('java.io.tmpdir'))
+
     def 'Processing successfully images'() {
         setup: 'Creating needed directories references'
-            def tmpDir = new File(System.getProperty('java.io.tmpdir'))
-            def imgDir = new File('src/test/resources/exercises/forwarding/images/')
-            def processor = new FileProcessorActor(
-                imageActor: new ImageProcessorActor(),
-                textActor: new TextProcessorActor()
-            )
-        and: 'Starting the monitoring process'
-            processor.with {
-                imageActor.start()
-                textActor.start()
-                start()
-            }
+            def monitorActor = new NotifiedActor(3).start()
+            def imageActor   = new ImageProcessorActor(monitorActor).start()
+            def textActor    = new TextProcessorActor(monitorActor).start()
+            def proxyActor   = new FileProcessorActor(imageActor: imageActor, textActor: textActor).start()
         when: 'Sending a couple of image files'
-            processor << new File(imgDir, 'image1.jpg')
-            processor << new File(imgDir, 'image2.jpg')
-            processor << new File(imgDir, 'text.txt')
-        then: 'We should make sure the have been transformed'
-            new File(tmpDir, 'processed_text.txt').exists()
-            new File(tmpDir, 'processed_image1.jpg').exists()
-            new File(tmpDir, 'processed_image2.jpg').exists()
+            proxyActor << new File(RESOURCES_DIR, 'image1.jpg')
+            proxyActor << new File(RESOURCES_DIR, 'image2.jpg')
+            proxyActor << new File(RESOURCES_DIR, 'text.txt')
+        and: 'Expecting process to finish'
+            monitorActor.join()
+        and: 'Looking for processed files'
+            def file1 = new File(TEMPORARY_DIR, 'processed_text.txt')
+            def file2 = new File(TEMPORARY_DIR, 'processed_image1.jpg')
+            def file3 = new File(TEMPORARY_DIR, 'processed_image2.jpg')
+        then: 'We should make sure they exist'
+            [file1, file2, file3].every { it.exists() }
+        cleanup: 'Deleting processed files'
+            [file1, file2, file3]*.delete()
     }
-
 }
